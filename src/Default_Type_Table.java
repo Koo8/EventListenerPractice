d89654bc91693122d78d0982bf3b6622264e95db
koo8 <kooeight@gmail.com>
2020-06-23 10:03:34 -0500

    simple default table model, tablemodel, tablemodelListener

---
+++ src/Default_Type_Table.java
@@ -0,0 +1,96 @@
+import javax.swing.*;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableModel;
+import java.awt.*;
+
+/**
+ * Use defaultTableModel
+ * See constructor part for its limited benefits
+ */
+public class Default_Type_Table extends JPanel implements TableModelListener {
+
+    // Constructor
+    Default_Type_Table() {
+        // super(new GridLayout(1, 0));
+        //declares the column names in a String array:
+        String[] columnNames = {"First Name",
+                "Last Name",
+                "Sport",
+                "# of Years",
+                "Vegetarian"};
+        //data is initialized and stored in a two-dimensional Object array:
+        Object[][] data = {
+                {"Kathy", "Smith",
+                        "Snowboarding", 5, Boolean.FALSE},
+                {"John", "Doe",
+                        "Rowing", 3, Boolean.TRUE},
+                {"Sue", "Black",
+                        "Knitting", 2, Boolean.FALSE},
+                {"Jane", "White",
+                        "Speed reading", 20, Boolean.TRUE},
+                {"Joe", "Brown",
+                        "Pool", 10, Boolean.FALSE}
+        };
+        // Jtable constructor
+        JTable table = new JTable(data, columnNames);
+        // table size
+        table.setPreferredScrollableViewportSize(new Dimension(700, 150));
+        // table take the entire height of the container
+        table.setFillsViewportHeight(true);
+
+        // create JScrollPane
+        // highlight: create a scrollpane as container for a table
+        // NOTE:oooo The scroll pane automatically places the table header at the top of the viewport.
+        // The column names remain visible at the top of the viewing area when the table data is scrolled.
+
+        JScrollPane pane = new JScrollPane(table);
+
+        add(pane);
+
+        //By default, all columns in a table start out with equal width
+        // to customize each column width, use the following code
+        // set the third column wider
+        TableColumn column = null;
+        for (int i = 0; i < table.getColumnCount(); i++) {
+            column = table.getColumnModel().getColumn(i);
+            if (i == 2) {
+                column.setPreferredWidth(100);
+            } else {
+                column.setPreferredWidth(50);
+            }
+        }
+
+        // add TableModelListener
+        table.getModel().addTableModelListener(this);
+    }
+
+
+    @Override // for TableModelListener, which is an interface implemented by JTable
+    public void tableChanged(TableModelEvent e) {
+        // TableModel is the source.
+        TableModel model = (TableModel) e.getSource();
+        Object celldata = model.getValueAt(e.getFirstRow(), e.getColumn());
+        System.out.println("Cell data is " + celldata);
+    }
+
+    public static void CreateAndShowGUI() {
+        JFrame frame = new JFrame(" MY Default Table - Simple ");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Default_Type_Table panel = new Default_Type_Table();
+
+        frame.setContentPane(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                CreateAndShowGUI();
+            }
+        });
+    }
+}
---
+++ src/TableDemo.java
@@ -0,0 +1,167 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.AbstractTableModel;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+/**
+ * TableDemo is just like SimpleTableDemo, except that it
+ * uses a custom TableModel.
+ */
+public class TableDemo extends JPanel {
+    private boolean DEBUG = true;
+
+    public TableDemo() {
+        super(new GridLayout(1,0));
+        // create a table with customized tableModel, to avoid 3 cons defaultTableModel has
+        JTable table = new JTable(new MyTableModel());
+        table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+        table.setFillsViewportHeight(true);
+
+        //Create the scroll pane and add the table to it.
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        //Add the scroll pane to this panel.
+        add(scrollPane);
+    }
+
+    class MyTableModel extends AbstractTableModel {
+
+        private String[] columnNames = {"First Name",
+                "Last Name",
+                "Sport",
+                "# of Years",
+                "Vegetarian"};
+        private Object[][] data = {
+                {"Kathy", "Smith",
+                        "Snowboarding", 5, Boolean.FALSE},
+                {"John", "Doe",
+                        "Rowing", 3, Boolean.TRUE},
+                {"Sue", "Black",
+                        "Knitting", 2, Boolean.FALSE},
+                {"Jane", "White",
+                        "Speed reading", 20, Boolean.TRUE},
+                {"Joe", "Brown",
+                        "Pool", 10, Boolean.FALSE}
+        };
+
+        @Override // tableModel
+        public int getColumnCount() {
+            return columnNames.length;
+        }
+        @Override // tableModel
+        public int getRowCount() {
+            return data.length;
+        }
+        @Override // tableModel
+        public String getColumnName(int col) {
+            return columnNames[col];
+        }
+        @Override // tableModel
+        public Object getValueAt(int row, int col) {
+            return data[row][col];
+        }
+
+        /* highlight: how to change String to boolean for cells
+         * JTable uses this method to determine the default renderer/
+         * editor for each cell.  If we didn't implement this method,
+         * then the last column would contain text ("true"/"false"),
+         * rather than a check box. --> compare with SimpleTableDemo.java
+         *
+         * Returns the most specific superclass for all the cell values in the column.
+         * This helps recognized column 4 to be integer and display right-aligned,
+         * This also help column 5 to display as checkbox for boolean.
+         */
+        @Override// tableModel
+        public Class getColumnClass(int c) {
+            return getValueAt(0, c).getClass();
+        }
+
+        /*
+         * Don't need to implement this method unless your table's
+         * editable.
+         * compare with SimpleTableDemo.java --> some cells are editable here vs.
+         * all are editable in simpleTableDemo
+         */
+        @Override // tableModel
+        public boolean isCellEditable(int row, int col) {
+            //Note that the data/cell address is constant,
+            //no matter where the cell appears onscreen.
+            if (col < 2) {
+                return false; // first and last name are not editable, others are editable
+            } else {
+                return true;
+            }
+        }
+//todo: where is the listener, only the boolean column can be listened
+        /*
+         * Don't need to implement this method unless your table's
+         * data can change.
+         */
+        public void setValueAt(Object value, int row, int col) {
+            if (DEBUG) {
+                System.out.println("Setting value at " + row + "," + col
+                        + " to " + value
+                        + " (an instance of "
+                        + value.getClass() + ")");
+            }
+
+            data[row][col] = value;
+            // There ara a few fire table data change event methods from JTable's DefaultTableModel instance
+            // inform all listeners that cell at this location is updated
+            fireTableCellUpdated(row, col);
+
+            if (DEBUG) {
+                System.out.println("New value of data:");
+                printDebugData();
+            }
+        }
+
+        private void printDebugData() {
+            int numRows = getRowCount();
+            int numCols = getColumnCount();
+
+            for (int i=0; i < numRows; i++) {
+                System.out.print("    row " + i + ":");
+                for (int j=0; j < numCols; j++) {
+                    System.out.print("  " + data[i][j]);
+                }
+                System.out.println();
+            }
+            System.out.println("--------------------------");
+        }
+    }
+
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("TableDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create and set up the content pane.
+        TableDemo newContentPane = new TableDemo();
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
